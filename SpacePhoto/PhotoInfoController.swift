c1ec6b1d76391e4f03995ab25661b8af5db7ac65
Jody Abney <jodyabney@gmail.com>
2020-05-14 22:44:29 -0400

    Complete except for scrollview not working

---
+++ SpacePhoto/PhotoInfoController.swift
@@ -0,0 +1,34 @@
+//
+//  PhotoInfoController.swift
+//  SpacePhoto
+//
+//  Created by Jody Abney on 5/14/20.
+//  Copyright © 2020 AbneyAnalytics. All rights reserved.
+//
+
+import Foundation
+
+class PhotoInfoController {
+    
+    func fetchPhotoInfo(completion: @escaping (PhotoInfo?) -> Void) {
+        
+        let baseURL = URL(string: "https://api.nasa.gov/planetary/apod")!
+        
+        let query: [String: String] = [
+            "api_key": "DEMO_KEY",
+        ]
+        
+        let url = baseURL.withQueries(query)!
+        let task = URLSession.shared.dataTask(with: url) {
+            (data, response, error) in
+            let decoder = JSONDecoder()
+            if let data = data, let photoInfo = try? decoder.decode(PhotoInfo.self, from: data) {
+                completion(photoInfo)
+            } else {
+                print("Either no data was returned, or data was not serialized.")
+                completion(nil)
+            }
+        }
+        task.resume()
+    }
+}
--- SpacePhoto/ViewController.swift
+++ SpacePhoto/ViewController.swift
@@ -9,12 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var nasaImage: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var copyrightLabel: UILabel!
+    
+    let photoInfoController = PhotoInfoController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        descriptionLabel.text = ""
+        copyrightLabel.text = ""
+        
+        
+        
+        photoInfoController.fetchPhotoInfo { (photoInfo) in
+            if let photoInfo = photoInfo {
+                self.updateUI(with: photoInfo)
+            }
+        }
     }
-
-
+    
+    func updateUI(with photoInfo: PhotoInfo) {
+        guard let url = photoInfo.url.withHTTPS() else { return }
+        
+        let task = URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
+            
+            guard let data = data, let image = UIImage(data: data) else { return }
+            DispatchQueue.main.async {
+                self.title = photoInfo.title
+                self.nasaImage.image = image
+                self.descriptionLabel.text = photoInfo.description
+                if let copyright = photoInfo.copyright {
+                    self.copyrightLabel.text = "Copyright \(copyright)"
+                } else {
+                    self.copyrightLabel.isHidden = true
+                }
+            }
+        })
+        
+        task.resume()
+    }
 }
 
